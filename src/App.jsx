39ca6d0fe19e1cea5447a14021146f2f069c4a30
Luv Gupta <lg5122003@gmail.com>
2023-09-09 23:30:31 +0530

    build error fix :alien:

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,25 @@ import GlobalContext from "./context/GlobalContext";
 import EventModal from "./components/EventModal";
 import HolidayEvent from "./HolidayEvent";
 
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
 
 function App() {
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCyZKBLUwE-5lJBj8aLhVqZnniAXlJwaJE",
-  //   authDomain: "chronoview-7a39a.firebaseapp.com",
-  //   projectId: "chronoview-7a39a",
-  //   storageBucket: "chronoview-7a39a.appspot.com",
-  //   messagingSenderId: "655782600676",
-  //   appId: "1:655782600676:web:1474d9a01940d0563b31f1",
-  //   measurementId: "G-Y2DQX1P6CW",
-  // };
-  // const app = initializeApp(firebaseConfig);
-  // const db = getFirestore(app);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCyZKBLUwE-5lJBj8aLhVqZnniAXlJwaJE",
+    authDomain: "chronoview-7a39a.firebaseapp.com",
+    projectId: "chronoview-7a39a",
+    storageBucket: "chronoview-7a39a.appspot.com",
+    messagingSenderId: "655782600676",
+    appId: "1:655782600676:web:1474d9a01940d0563b31f1",
+    measurementId: "G-Y2DQX1P6CW",
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
-  // console.log(db);
 
   const [currenMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
-
-  // const messagesRef = firestore.collection("tasks");
-  // const query = messagesRef.orderBy("createdAt").limit(25);
-
-  // const [messages] = useCollectionData(query, { idField: "id" });
 
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
